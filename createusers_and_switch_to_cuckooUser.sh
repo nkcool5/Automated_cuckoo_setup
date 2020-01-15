f54fae4323a64236d975cf20b1032c8b932e3e38
Nitesh Kumar <32159737+nkcool5@users.noreply.github.com>
2020-01-15 16:18:36 +0530

    Delete createusers_and_switch_to_cuckooUser.sh

--- createusers_and_switch_to_cuckooUser.sh
+++
@@ -1,9 +0,0 @@
-echo "==================================================================================="
-echo "				      ADD CUCKOO USER 					 "
-echo "==================================================================================="
-echo "Enter Username To Create New User For Cuckoo Installation"
-read username
-sudo adduser $username
-sudo usermod -aG sudo $username
-echo "==================================="
-echo ""
